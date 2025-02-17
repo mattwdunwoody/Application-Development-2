17e7fe9bf1d9488e6d876e0992ac594ffdaafc3a
mattwdunwoody <widmatg@dunwoody.edu>
2025-02-17 10:37:35 -0600

    second commit, lab 01

--- AD2 Homework 1/GadgetHub/GadgetHub.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ AD2 Homework 1/GadgetHub/GadgetHub.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -6,6 +6,7 @@ using Ninject;
 using Moq;
 using GadgetHub.Domain.Abstract;
 using GadgetHub.Domain.Entities;
+using GadgetHub.Domain.Concrete;
 
 namespace GadgetHub.WebUI.Infrastructure
 {
@@ -31,13 +32,14 @@ namespace GadgetHub.WebUI.Infrastructure
 
 		public void AddBindings()
 		{
-			Mock<IGadgetRepository> mock = new Mock<IGadgetRepository>();
+			/*Mock<IGadgetRepository> mock = new Mock<IGadgetRepository>();
 			mock.Setup(m => m.Gadgets).Returns(new List<Gadget> {
 				new Gadget{ GadgetName = "MacBook Air", GadgetBrand = "Apple", GadgetPrice = 200, GadgetDesc = "A simple no-frills work laptop.", GadgetCategory = "Computers" },
 				new Gadget{ GadgetName = "Fenix 7", GadgetBrand = "Garmin", GadgetPrice = 500, GadgetDesc = "A GPS-Enable smartwatch. Charges with solar power.", GadgetCategory = "Wearables" },
 				new Gadget{ GadgetName = "Beats 7", GadgetBrand = "Dr. Dre", GadgetPrice = 100, GadgetDesc = "High-Fidelity over-ear headphones.", GadgetCategory = "Audio" }
 			});
-			kernel.Bind<IGadgetRepository>().ToConstant(mock.Object);
+			kernel.Bind<IGadgetRepository>().ToConstant(mock.Object);*/
+			kernel.Bind<IGadgetRepository>().To<EFGadgetRepository>();
 		}
 	}
 }
